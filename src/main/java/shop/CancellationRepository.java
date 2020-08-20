72935aba1e1ab14aeb79c0b3a03514673bb191b4
yooyoungjun <iamakira83@gmail.com>
2020-08-20 16:41:32 +0900

    init commit..

---
+++ src/main/java/shop/CancellationRepository.java
@@ -0,0 +1,8 @@
+package shop;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CancellationRepository extends PagingAndSortingRepository<Cancellation, Long>{
+
+
+}
---
+++ src/main/java/shop/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package shop;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
+
+
+}
